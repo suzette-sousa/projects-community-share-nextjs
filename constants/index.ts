3edfbd6e826295c27033ec132e64ce46bfce8ffa
Suzette Sousa <sousa.suzette.pro@gmail.com>
2023-06-26 15:11:00 +0200

    add footer

--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,74 @@ export const NavLinks = [
   { href: '/', key: 'Carrière', text: 'Carrière' },
   { href: '/', key: 'Emploi', text: 'Emploi' },
 ];
+
+export const footerLinks = [
+  {
+    title: 'Pour les développeurs',
+    links: [
+      'Passe à la version pro !',
+      'Découvre les projets',
+      'Blog',
+      'Podcast',
+      'Projets open-source',
+      'Parraine un ami',
+      'Code de conduite',
+    ],
+  },
+  {
+    title: 'Embaucher un développeur',
+    links: [
+      'Poster une annonce',
+      'Poster un projet pour freelance',
+      'Recherche de développeurs',
+    ],
+  },
+  {
+    title: 'Marques',
+    links: ['Faites votre promotion'],
+  },
+  {
+    title: 'Flexibble',
+    links: [
+      'À propos',
+      'Carrières',
+      'Aide',
+      'Kit médias',
+      'Témoignages',
+      'API',
+      'CGU',
+      'Politique de confidentialité',
+      'Utilisation des cookies',
+    ],
+  },
+  {
+    title: 'Annuaires',
+    links: [
+      'Jobs',
+      'Développeurs disponibles',
+      'Développeurs freelance disponibles',
+      'Mots clés',
+      'Lieux',
+    ],
+  },
+  {
+    title: 'Outils de développement',
+    links: [
+      'Marketplace',
+      'GitHub Marketplace',
+      'NPM Registry',
+      'Packagephobia',
+    ],
+  },
+  {
+    title: 'Ressources',
+    links: [
+      'Freelance',
+      'Emploi',
+      'Portfolio',
+      'Education',
+      'Process créatif',
+      'Tendances',
+    ],
+  },
+];
