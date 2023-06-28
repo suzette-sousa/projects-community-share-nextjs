ba357c08b9bed8ac269c7761d51c004620f1722c
Suzette Sousa <sousa.suzette.pro@gmail.com>
2023-06-28 10:28:00 +0200

    add infos projects card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 type Props = {
   id: string;
@@ -11,6 +14,14 @@ type Props = {
 };
 
 const ProjectCard = ({ id, image, title, name, avatarUrl, userId }: Props) => {
+  const [randomLikes, setRandomLikes] = useState(0);
+  const [randomViews, setRandomViews] = useState('');
+
+  useEffect(() => {
+    setRandomLikes(Math.floor(Math.random() * 10000));
+    setRandomViews(Math.floor(Math.random() * 10000).toFixed(1) + 'k');
+  }, []);
+
   return (
     <div className="flexCenter flex-col rounded-2xl drop-shadow-card ">
       <Link
@@ -24,7 +35,38 @@ const ProjectCard = ({ id, image, title, name, avatarUrl, userId }: Props) => {
           className="w-full h-full object-cover rounded-2xl"
           alt="Image du projet"
         />
+
+        <div className="hidden group-hover:flex profile_card-title">
+          <p className="w-full">{title}</p>
+        </div>
       </Link>
+
+      <div className="flexBetween w-full px-2 mt-3 font-semibold text-sm">
+        <Link href={`/project/${userId}`}>
+          <div className="flexCenter gap-2">
+            <Image
+              src={avatarUrl}
+              width={24}
+              height={24}
+              className="rounded-full"
+              alt="Image profil"
+            />
+            <p>{name}</p>
+          </div>
+        </Link>
+
+        <div className="flexCenter gap-3">
+          <div className="flexCenter gap-2">
+            <Image src="/hearth.svg" width={13} height={12} alt="Likes" />
+            <p className="text-sm">{randomLikes}</p>
+          </div>
+
+          <div className="flexCenter gap-2">
+            <Image src="/eye.svg" width={13} height={12} alt="Vues" />
+            <p className="text-sm">{randomViews}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
